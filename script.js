8c592f7e8afcd251b9ff69b961e9ef13ba5c0968
Matthew Pierce <lesbianhaircut@outlook.com>
2021-09-16 21:39:33 -0400

    c

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-window.onload = () => {};
+window.onload = () => {
+  // Variables
+  getStartedButton = document.querySelector(".submit");
+  emailField = document.querySelector(".email");
+
+  // Functions
+  function checkEmail() {
+    const re = console.log(re.test(emailField.textContent));
+  }
+
+  // Event Listeners
+  getStartedButton.addEventListener("click", checkEmail);
+};
